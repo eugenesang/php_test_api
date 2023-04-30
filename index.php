9c71e5da7a01b80d1b2dc136cf398e30ae69f754
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2023-04-30 12:09:21 +0300

    Blend frontend

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-$response = array(
-    "success" => true,
-    "message" => "Api endpoint was successfully connected"
-);
+include('index.html');
 
-echo json_encode($response);
-
-exit;
 ?>
